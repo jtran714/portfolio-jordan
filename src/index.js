5db82cc11e21891efda9203c6245f2a279ed8def
Jordan Tran <thai.jordan@gmail.com>
2023-02-12 13:11:59 -0800

    cleaned up some code on index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// https://reactjs.org/docs/context.html
 import { AppProvider } from "./appContext";
 // https://redux.js.org/tutorials/fundamentals/part-5-ui-react#passing-the-store-with-provider
 import { Provider } from "react-redux";
 import { store } from "./store";
-// https://create-react-app.dev/docs/adding-bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
